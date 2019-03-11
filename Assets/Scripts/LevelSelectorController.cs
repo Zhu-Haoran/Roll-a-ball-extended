915ca2e0b3ced2fdfd57c9dfb514961632ca62f4
digif <digifi@laposte.net>
2019-03-11 15:21:39 +0100

    level selection done, added build, final commit

---
+++ Assets/Scripts/LevelSelectorController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectorController : MonoBehaviour
+{
+    public void OpenLevel(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
